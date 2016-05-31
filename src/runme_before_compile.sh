8ea06e15708df7985583b36a1d9cafcc25274ead
Guillermo <gufranco@mail.wvu.edu>
2016-05-31 17:35:36 -0400

    moving tests into results, ignoring results

--- src/runme_before_compile.sh
+++ src/runme_before_compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+MASTER=fireball-master
+
 for i in a.GLOBAL b.FUNCTIONS c.SYSTEM d.FUNCTIONS_EXTRA e.FDATA g.XC_FUNCTIONALS h.SOLVESH i.GRID j.ASSEMBLERS l.SCF p.HARRIS q.DOGS o.OUTPUT u.UTIL x.EXECUTABLES Makefile MACHINES libs include
 do
-    if [ ! -e $i ]
+    if [ -e $i ]
     then
-	ln -s ../../fireball-thunder/src/$i
+	rm $i
     fi
+    ln -s ../../${MASTER}/src/$i
 done
